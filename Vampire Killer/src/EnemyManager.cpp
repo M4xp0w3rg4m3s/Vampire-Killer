e46274436dd37bc7179d8e0bee83c371c88edb85
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2024-04-21 02:03:51 +0200

    Update EnemyManager.cpp

--- Vampire Killer/src/EnemyManager.cpp
+++ Vampire Killer/src/EnemyManager.cpp
@@ -10,8 +10,28 @@ void EnemyManager::Update()
 		enemies[i].Update();
 	}
 }
-
-
+void EnemyManager::SetEnemy(int id, EnemyType type)
+{
+	enemies[id].SetType(type);
+}
+void EnemyManager::SpawnEnemy(EnemyType type)
+{
+	for (int i = 0; i < 10; ++i) {
+		if (enemies[i].GetType() == type) {
+			enemies[i].Spawn();
+			break;
+		}
+	}
+}
+void EnemyManager::SpawnEnemy(EnemyType type, Point pos)
+{
+	for (int i = 0; i < 10; ++i) {
+		if (enemies[i].GetType() == type) {
+			enemies[i].Spawn(pos);
+			break;
+		}
+	}
+}
 void EnemyManager::Render()
 {
 	for (int i = 0; i < 10; ++i) {
