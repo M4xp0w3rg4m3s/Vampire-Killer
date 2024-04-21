0c8e2aa75a9bfb0ce556a1ba98050bb74728b8b0
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2024-04-21 14:25:03 +0200

    objects, initial

--- Vampire Killer/src/Object.h
+++ Vampire Killer/src/Object.h
@@ -1,24 +1,22 @@
 #pragma once
 #include "Entity.h"
 
-#define OBJECT_PHYSICAL_SIZE	14
-#define OBJECT_FRAME_SIZE		16
+#define OBJECT_PHYSICAL_SIZE    14
+#define OBJECT_FRAME_SIZE        16
 
-#define POINTS_APPLE	10
-#define POINTS_CHILI	20
+#define POINTS_HEART    0
 
-enum class ObjectType { APPLE, CHILI };
+enum class ObjectType { CHAIN };
 
 class Object : public Entity
 {
 public:
-	Object(const Point& p, ObjectType t);
-	~Object();
+    Object(const Point& p, ObjectType t);
+    ~Object();
 
-	void DrawDebug(const Color& col) const;
-	int Points() const;
+    void DrawDebug(const Color& col) const;
+    int Points() const;
 
 private:
-	ObjectType type;
+    ObjectType type;
 };
-
